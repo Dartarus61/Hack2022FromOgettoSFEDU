7f5e73721a2ee33f81009c4c27c931bddf6c8764
Dartarus61 <76129308+Dartarus61@users.noreply.github.com>
2022-11-26 16:11:26 +0300

    upd: swgger

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,53 +1,64 @@
-import { Body, Controller, Get, Headers, Param, Post, Put, Redirect, UseGuards } from '@nestjs/common'
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { ChangeRoleDto } from './dto/ChangeRole.dto'
-import { CreateUserDto } from './dto/create_user.dto'
-import { UpdateUserDto } from './dto/UpdateUser.dto'
-import { User } from '../models/user.model'
-import { UserService } from './user.service'
-import { Roles } from 'src/auth/roles-auth.decorator'
-import { RolesGuard } from 'src/auth/roles.guard'
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Put,
+  Redirect,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ChangeRoleDto } from './dto/ChangeRole.dto';
+import { CreateUserDto } from './dto/create_user.dto';
+import { UpdateUserDto } from './dto/UpdateUser.dto';
+import { User } from '../models/user.model';
+import { UserService } from './user.service';
+import { Roles } from 'src/auth/roles-auth.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 @ApiTags('Пользователи')
 @Controller('user')
 export class UserController {
-    constructor(private UserService: UserService) {}
+  constructor(private UserService: UserService) {}
 
-    @Get('/activ/:value')
-    @Redirect('http://localhost:3000/login')
-    async activation(@Param('value') value: string) {
-        const fuser = await this.UserService.activate(value)
-        return { url: 'http://localhost:3000/' }
-    }
+  @Get('/activ/:value')
+  @Redirect('http://localhost:3000/login')
+  async activation(@Param('value') value: string) {
+    const fuser = await this.UserService.activate(value);
+    return { url: 'http://localhost:3000/' };
+  }
 
-    @ApiOperation({ summary: 'Поиск пользователя по почте' })
-    @ApiResponse({ status: 200, type: User })
-    @UseGuards(RolesGuard)
-    @Roles('USER')
-    @Post('/byemail')
-    GetUserByEmail(@Body('email') email: string) {
-        return this.UserService.getUserByEmail(email)
-    }
+  @ApiOperation({ summary: 'Поиск пользователя по почте' })
+  @ApiResponse({ status: 200, type: User })
+  @UseGuards(RolesGuard)
+  @Roles('USER')
+  @Post('/byemail')
+  GetUserByEmail(@Body('email') email: string) {
+    return this.UserService.getUserByEmail(email);
+  }
 
-    @ApiOperation({ summary: 'Выдать роль' })
-    @ApiResponse({ status: 200 })
-    @UseGuards(RolesGuard)
-    @Roles('ADMIN')
-    @Post('/addrole')
-    ChangeRole(@Body() dto: ChangeRoleDto) {
-        return this.UserService.addRole(dto)
-    }
+  @ApiOperation({ summary: 'Выдать роль' })
+  @ApiResponse({ status: 200 })
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
+  @Post('/addrole')
+  ChangeRole(@Body() dto: ChangeRoleDto) {
+    return this.UserService.addRole(dto);
+  }
 
-    @ApiOperation({ summary: 'Получить всех пользователей' })
-    @ApiResponse({ status: 200, type: [User] })
-    @UseGuards(RolesGuard)
-    @Roles('ADMIN')
-    @Get('/all')
-    AllPeople() {
-        return this.UserService.getAll()
-    }
+  @ApiOperation({ summary: 'Получить всех пользователей' })
+  @ApiResponse({ status: 200, type: [User] })
+  @UseGuards(RolesGuard)
+  @Roles('ADMIN')
+  @Get('/all')
+  AllPeople() {
+    return this.UserService.getAll();
+  }
 
-    @Get('/profile')
-    userProfile(@Headers('authorization') authorization: string) {
-        return this.UserService.GetMyProfile(authorization)
-    }
+  @ApiOperation({ summary: 'Профиль пользователя' })
+  @Get('/profile')
+  userProfile(@Headers('authorization') authorization: string) {
+    return this.UserService.GetMyProfile(authorization);
+  }
 }
